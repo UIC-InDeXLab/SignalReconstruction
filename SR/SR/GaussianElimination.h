f97fb7bed783fe9a82f9cc78cb7526e96892cfcc
asudeh <a.asudeh@gmail.com>
2018-09-29 14:34:58 -0400

    ;

--- SR/SR/GaussianElimination.h
+++ SR/SR/GaussianElimination.h
@@ -1,29 +1,17 @@
-// C++ Implementation for Gauss-Jordan, modified from https://www.geeksforgeeks.org/program-for-gauss-jordan-elimination-method/
-// Elimination Method 
+// Abolfazl Asudeh; http://asueh.github.io
+// Gaussian Elimination method for solving Ax=b
+// The code is a modification from https://www.geeksforgeeks.org/program-for-gauss-jordan-elimination-method/
+
 using namespace std;
 #include<iostream>
 
 #include "MyMatrix.h"
 
-#define M 10 
-
-// Function to print the matrix 
-void PrintMatrix(float a[][M], int n)
+int Solve(Matrix A, vector<float> b, float* result) // solves the equation Ax = b
 {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= n; j++)
-			cout << a[i][j] << " ";
-		cout << endl;
-	}
-}
-
-// function to reduce matrix to reduced 
-// row echelon form. 
-int PerformOperation(Matrix A, vector<float> b) // solves the equation Ax = b
-{
-	int n = A.n, i, j, k = 0, c, flag = 0, m = 0;
+	int n = A.n, i, j, k = 0, kp = 0, c, flag = 0, m = 0;
 	float pro = 0;
-
+	result = new float[n];
 	int* pointers = new int[n];
 	for (i = 0; i < n; i++) pointers[i] = 0;
 
@@ -40,66 +28,40 @@ int PerformOperation(Matrix A, vector<float> b) // solves the equation Ax = b
 				flag = 1;
 				break;
 			}
-			for (j = i, k = 0; k <= n; k++)
-				swap(a[j][k], a[j + c][k]);
+			//for (j = i, k = 0; k <= n; k++)
+			//	swap(a[j][k], a[j + c][k]);
+			swap(A.rows[i], A.rows[i + c]);
+			swap(b[i], b[i + c]);
 		}
 
-		for (j = 0; j < n; j++) {
+		result[i] = A.rows[i + c][pointers[i]].value;
 
+		for (j = 0; j < n; j++) {
 			// Excluding all i == j 
 			if (i != j) {
-
+				k = pointers[i]; kp = pointers[j];
 				// Converting Matrix to reduced row 
 				// echelon form(diagonal matrix) 
-				float pro = a[j][i] / a[i][i];
+				if (kp = A.rows[j].size() || A.rows[j][kp].index > i) continue; // A[j][i] is already zero
+				// if A[j][i]!=0. Note: A.rows[j][kp].index cannot be less than i
+				float pro = A.rows[j][kp].value / A.rows[i][k].value;
 
-				for (k = 0; k <= n; k++)
-					a[j][k] = a[j][k] - (a[i][k]) * pro;
+				// For all columns forward update the weights --> start from the pointers[j] and continue
+				while (k < A.rows[i].size() && kp < A.rows[j].size())
+					if (A.rows[i][k].index>A.rows[j][kp].index) kp++;
+					else if (A.rows[i][k].index < A.rows[j][kp].index) k++;
+					else
+					{
+						// a[j][k] = a[j][k] - (a[i][k]) * pro;
+						A.rows[j][kp].value -= pro * A.rows[i][k].value;
+						k++; kp++;
+					}
 			}
 		}
 	}
-	return flag;
-}
 
-// Function to print the desired result  
-// if unique solutions exists, otherwise  
-// prints no solution or infinite solutions  
-// depending upon the input given. 
-void PrintResult(float a[][M], int n, int flag)
-{
-	cout << "Result is : ";
+	for (int i = 0; i < n; i++)
+			result[i] = b[i] / result[i];
 
-	if (flag == 2)
-		cout << "Infinite Solutions Exists" << endl;
-	else if (flag == 3)
-		cout << "No Solution Exists" << endl;
-
-
-	// Printing the solution by dividing constants by 
-	// their respective diagonal elements 
-	else {
-		for (int i = 0; i < n; i++)
-			cout << a[i][n] / a[i][i] << " ";
-	}
-}
-
-// To check whether infinite solutions  
-// exists or no solution exists 
-int CheckConsistency(float a[][M], int n, int flag)
-{
-	int i, j;
-	float sum;
-
-	// flag == 2 for infinite solution 
-	// flag == 3 for No solution 
-	flag = 3;
-	for (i = 0; i < n; i++)
-	{
-		sum = 0;
-		for (j = 0; j < n; j++)
-			sum = sum + a[i][j];
-		if (sum == a[i][j])
-			flag = 2;
-	}
 	return flag;
 }
